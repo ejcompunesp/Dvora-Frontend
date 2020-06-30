25674a85f5c06023bdcca023d5acf52c3c60f432
JP Baptista <joaosbaptista@hotmail.com>
2020-06-30 13:01:50 -0300

    retirando console log da chamada

--- src/api/members.js
+++ src/api/members.js
@@ -1,11 +1,7 @@
 import api from './api';
 
 const membersApi = {
-  store: (jeId, data) => {
-    for (var value of data.values()) {    
-      console.log(value);  
-    }
-    return api.post(`/jes/${jeId}/members/signup`, data, {headers:{'Content-Type': 'multipart/form-data'}})},
+  store: (jeId, data) => api.post(`/jes/${jeId}/members/signup`, data, {headers:{'Content-Type': 'multipart/form-data'}}),
   list: (jeId) => api.get(`/jes/${jeId}/members`),
   delete: (jeId, memberId) => api.delete(`jes/${jeId}/members/delete`, { data : memberId }),
   update: (jeId, data) => api.put(`jes/${jeId}/members/update`, data)
