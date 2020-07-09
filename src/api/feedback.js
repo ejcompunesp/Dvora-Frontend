3214a490b6ccd1a7854e606b32dcf14dfe8a86fc
JP Baptista <joaosbaptista@hotmail.com>
2020-07-08 23:47:48 -0300

    feat: conexao com a api

--- src/api/feedback.js
+++ src/api/feedback.js
@@ -3,6 +3,7 @@ import api from './api';
 const feedbacksApi = {
   create : (dutyId, data) => api.post(`/duties/${dutyId}/feedback`, data),
   index : (jeId) => api.get('/feedback', { data: jeId }),
+  get : (memberId) => api.get(`/member/${memberId}/feedback`, )
 };
 
 export default feedbacksApi;
