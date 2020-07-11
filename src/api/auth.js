fece7aa516225baddfa3addbad3e65d5a4f1b712
JP Baptista <joaosbaptista@hotmail.com>
2020-07-11 20:57:35 -0300

    fix: corrigindo verificação de membro

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,9 +6,11 @@ export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const isLoginMember = () => {
   const user = JSON.parse(localStorage.getItem("state"));
   if(user) {
-		if (user.member.id) {
-  	  return true; 
- 	  }
+    if(user.member){
+      if (user.member.id) {
+        return true; 
+       }
+    }
 	}
   return false;
 };
