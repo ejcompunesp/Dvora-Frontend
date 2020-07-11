c85f155351990c85949f20f8510c78d6c5b1f9c6
JP Baptista <joaosbaptista@hotmail.com>
2020-07-11 20:57:50 -0300

    feat: dinamizando avaliação do membro no monitoramento

--- src/components/common/monitoring/FeedbackMonitoring.js
+++ src/components/common/monitoring/FeedbackMonitoring.js
@@ -1,41 +1,57 @@
-import React from "react";
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 
 import { Rate } from "antd";
 
-import { FeedbackContainer, FeedbackTitle, FeedbackContent, QuestionContainer } from "./styles/monitoring";
+import {
+  FeedbackContainer,
+  FeedbackTitle,
+  FeedbackContent,
+  QuestionContainer,
+} from "./styles/monitoring";
 
 const StyledRate = styled(Rate)`
-  color: #00A7E1;
+  color: #00a7e1;
 `;
 
-const FeedbackMonitoring = () => {
+const FeedbackMonitoring = ({ duties, dutyId }) => {
+  const [duty, setDuty] = useState([]);
+  useEffect(() => {
+    if (dutyId !== null) setDuty(duties.filter((item) => item.id === dutyId));
+  }, [dutyId]);
   return (
     <FeedbackContainer>
       <FeedbackTitle>
-        <h2>Notas</h2>
+        <h2>Avaliação</h2>
       </FeedbackTitle>
       <FeedbackContent>
-        <QuestionContainer>
-          <h4>Como você acredita que foi seu rendimento?</h4>
-          <StyledRate disabled defaultValue={4} />
-        </QuestionContainer>
-        <QuestionContainer>
-          <h4>Como você acredita que foi seu rendimento?</h4>
-          <StyledRate disabled defaultValue={3} />
-        </QuestionContainer>
-        <QuestionContainer>
-          <h4>Como você acredita que foi seu rendimento?</h4>
-          <StyledRate disabled defaultValue={2} />
-        </QuestionContainer>
-        <QuestionContainer>
-          <h4>Como você acredita que foi seu rendimento?</h4>
-          <StyledRate disabled defaultValue={2} />
-        </QuestionContainer>
-        <QuestionContainer>
-          <h4>Como você acredita que foi seu rendimento?</h4>
-          <StyledRate disabled defaultValue={2} />
-        </QuestionContainer>
+        {duty.length > 0 ? (
+          <>
+            {duty[0].feedback !== null ? (
+              <>
+              <QuestionContainer>
+              <h4>Qual seu nível de satisfação com a empresa?</h4>
+              <StyledRate disabled defaultValue={duty[0].feedback.satisfaction} />
+            </QuestionContainer>
+            <QuestionContainer>
+              <h4>Como você acredita que foi seu rendimento?</h4>
+              <StyledRate disabled defaultValue={duty[0].feedback.productivity} />
+            </QuestionContainer>
+            <QuestionContainer>
+              <h4>Como você avalia seu humor hoje?</h4>
+              <StyledRate disabled defaultValue={duty[0].feedback.mood} />
+            </QuestionContainer>
+              </>
+            ) : 
+              <h1>Membro não fez o feedback de seu plantão</h1>
+            }
+            
+          </>
+        ) : (
+          <h1>
+            Selecione um dia para a análise
+          </h1>
+        )}
       </FeedbackContent>
     </FeedbackContainer>
   );
