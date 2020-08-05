11ff88d3fb388c87eb006cc6c119361d4592b731
Karen <karenkaka2010@hotmail.com>
2020-08-05 19:35:39 -0300

    alterando rota de feedback

--- src/api/feedback.js
+++ src/api/feedback.js
@@ -3,7 +3,7 @@ import api from './api';
 const feedbacksApi = {
   create : (dutyId, data) => api.post(`/duties/${dutyId}/feedback`, data),
   get : (memberId) => api.get(`/member/${memberId}/feedback`, ),
-  index : (jeId) => api.get(`/feedback/${jeId}`),
+  index : () => api.get(`/feedback`),
 };
 
 export default feedbacksApi;
