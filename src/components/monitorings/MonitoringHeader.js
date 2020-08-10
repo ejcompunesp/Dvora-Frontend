7da8715f3db69b5c8216103663bccda6d3433032
JP Baptista <joaosbaptista@hotmail.com>
2020-08-09 23:26:31 -0300

    feat: passando info entre componentes sobre o feedback

--- src/components/monitorings/MonitoringHeader.js
+++ src/components/monitorings/MonitoringHeader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Title } from "../../views/dashboard/team/styles/team";
 import { ProfilePhoto } from "./styles/monitoring";
@@ -6,8 +6,19 @@ import { ProfilePhoto } from "./styles/monitoring";
 import imgTest from "../../assets/John Doe.png";
 import { Button } from "antd";
 
-const MonitoringHeader = ({ name, role, img }) => {
-  const apiURL = 'https://backend-dvora.herokuapp.com/files/member';
+const MonitoringHeader = ({ name, role, img, feedback }) => {
+  const apiURL = "https://backend-dvora.herokuapp.com/files/member";
+  const [disabled, setDisabled] = useState(true);
+
+  const getDisabled = (feedback) => {
+    if(feedback === null) return true;
+    return false;
+  }
+
+  useEffect(() => {
+    setDisabled(getDisabled(feedback));
+  }, [feedback])
+
   return (
     <>
       <Title>
@@ -15,18 +26,18 @@ const MonitoringHeader = ({ name, role, img }) => {
           style={{
             display: "flex",
             justifyContent: "space-between",
-            alignItems: 'center',
+            alignItems: "center",
             width: "100%",
           }}
         >
-          <div style={{display: 'flex'}}>
+          <div style={{ display: "flex" }}>
             <ProfilePhoto src={img ? `${apiURL}/${img}` : imgTest} />
             <div style={{ display: "flex", flexDirection: "column" }}>
               <h2>{name} - Acompanhamento</h2>
               <h3>{role}</h3>
             </div>
           </div>
-          <Button type="primary">Concluir Acompnhamento</Button>
+          <Button type="primary" disabled={disabled} >Concluir Acompnhamento</Button>
         </div>
       </Title>
     </>
