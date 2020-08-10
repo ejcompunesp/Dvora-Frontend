321efb35df3fb1e95bb599a50e0b6643a5c6e0d5
JP Baptista <joaosbaptista@hotmail.com>
2020-08-09 23:25:28 -0300

    feat: nova rota

--- src/api/feedback.js
+++ src/api/feedback.js
@@ -3,7 +3,8 @@ import api from './api';
 const feedbacksApi = {
   create : (dutyId, data) => api.post(`/duties/${dutyId}/feedback`, data),
   get : (memberId) => api.get(`/member/${memberId}/feedback`, ),
-  index : (jeId) => api.get(`/feedback/${jeId}`),
+  put : (feedbackId) => api.put(`/duties/feedback/monitoring`, { feedbackId }),
+  index : (jeId) => api.get(`/feedback`),
 };
 
 export default feedbacksApi;
